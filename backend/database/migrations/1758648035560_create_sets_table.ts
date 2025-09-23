066fda40ebe3a3d38e76008665477d418273b2cd
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-09-23 19:56:53 +0200

    feat : Ajout des migrations principales

---
+++ backend/database/migrations/1758648035560_create_sets_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'sets'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('reps')
+      table.integer('weight')
+      table.string('comment')
+      table.integer('restTime')
+      table.string('tempo')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
