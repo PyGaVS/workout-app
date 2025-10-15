969d76f296ead91d8af01b9bce6b08e88c321370
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-10-16 01:58:29 +0200

    feat(workout) : implémentation création d'un workout

--- backend/app/domains/workout/controllers/workouts_controller.ts
+++ backend/app/domains/workout/controllers/workouts_controller.ts
@@ -1,8 +1,7 @@
-// import type { HttpContext } from '@adonisjs/core/http'
-
 import type { HttpContext } from '@adonisjs/core/http'
-import WorkoutService from '#domains/workout/services/workouts_services'
+import WorkoutService from '#domains/workout/services/workouts_service'
 import { inject } from '@adonisjs/core'
+import { createWorkoutValidator } from '#domains/workout/validators/workouts_validator'
 
 @inject()
 export default class WorkoutsController {
@@ -10,5 +9,13 @@ export default class WorkoutsController {
 
   index({ auth }: HttpContext) {
     return this.workoutService.getWorkouts(auth.user!.id)
+  }
+
+  async store({ auth, request, response }: HttpContext) {
+    const userId = auth.user!.id
+    const data = await request.validateUsing(createWorkoutValidator)
+    const workout = await this.workoutService.create({ ...data }, userId)
+
+    return response.created(workout)
   }
 }
