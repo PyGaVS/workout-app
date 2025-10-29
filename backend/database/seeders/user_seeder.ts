7f17f46871d3f75052df1f742394743cbcf6039a
alexandrebrgn <brgn.alexandre@gmail.com>
2025-10-29 12:27:09 +0100

    feat(commands) : changement pour la commande make:user

--- backend/database/seeders/user_seeder.ts
+++ backend/database/seeders/user_seeder.ts
@@ -1,9 +1,17 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import db from '@adonisjs/lucid/services/db'
 import hash from '@adonisjs/core/services/hash'
+import { Permissions } from '#commons/permissions/permissions_enum'
+import { computeBitfield } from '#commons/utils/compute_bitfield'
 
 export default class extends BaseSeeder {
   async run() {
+    const defaultUserPermissions = [
+      Permissions.CREATE_WORKOUT,
+      Permissions.DELETE_WORKOUT,
+      Permissions.EDIT_WORKOUT,
+    ]
+
     await db
       .insertQuery()
       .table('users')
@@ -11,6 +19,7 @@ export default class extends BaseSeeder {
         full_name: 'Alexandre Bourguignon',
         email: 'alex.brgn@workout.fr',
         password: await hash.make('12345678'),
+        permissions: computeBitfield(defaultUserPermissions),
         created_at: new Date(),
         updated_at: new Date(),
       })
@@ -21,6 +30,7 @@ export default class extends BaseSeeder {
         full_name: 'Lylian Ball',
         email: 'lyl.ball@workout.fr',
         password: await hash.make('12345678'),
+        permissions: computeBitfield(defaultUserPermissions),
         created_at: new Date(),
         updated_at: new Date(),
       })
