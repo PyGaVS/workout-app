85d06daa72fa4020cefb9dc402fa0ec665d407a5
alexandrebrgn <brgn.alexandre@gmail.com>
2025-10-31 01:29:22 +0100

    feat(policy) : policies workout

--- backend/app/domains/workout/policies/workout_policy.ts
+++ backend/app/domains/workout/policies/workout_policy.ts
@@ -11,27 +11,35 @@ export default class WorkoutPolicy extends BasePolicy {
     super()
   }
 
-  async browse() {
-    return true
+  // If user is admin, he is authorized anyway
+  async before(user: User) {
+    if (await this.permissionService.hasPermission(user, Permissions.ADMIN)) {
+      return true
+    }
   }
 
+  // User is able to browse workouts (maybe to get inspiration)
+  async browse(user: User) {
+    return this.permissionService.hasPermission(user, Permissions.VIEW_WORKOUT)
+  }
+
+  // User is able to create a workout
   async create(user: User) {
-    return this.permissionService.hasPermission(user, Permissions.CREATE_WORKOUT)
+    return this.permissionService.hasPermission(user, Permissions.MANAGE_WORKOUT)
   }
 
+  // User is able to see his own workouts
   async view(user: User, workout: Workout) {
     return user.id === workout.userId
   }
 
+  // User is able to edit his own workouts and only his own workouts
   async edit(user: User, workout: Workout) {
-    const isOwner = user.id === workout.userId
-    const canEdit = await this.permissionService.hasPermission(user, Permissions.EDIT_WORKOUT)
-    return isOwner || canEdit
+    return user.id === workout.userId
   }
 
+  // User is able to delete his own workouts and only his own workouts
   async delete(user: User, workout: Workout) {
-    const isOwner = user.id === workout.userId
-    const canDelete = await this.permissionService.hasPermission(user, Permissions.DELETE_WORKOUT)
-    return isOwner || canDelete
+    return user.id === workout.userId
   }
 }
