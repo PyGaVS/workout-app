5f5b52c873bc76fc8b98386f5f2e185587af7e0c
alexandrebrgn <brgn.alexandre@gmail.com>
2025-11-02 02:46:02 +0100

    feat(policy) : user policies

---
+++ backend/app/domains/user/router.ts
@@ -0,0 +1,17 @@
+import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+
+const UsersController = () => import('#domains/user/controllers/users_controller')
+
+export default function userRoutes() {
+  router
+    .group(() => {
+      router
+        .group(() => {
+          router.get('/:userId', [UsersController, 'show'])
+        })
+        .prefix('/users')
+    })
+    .prefix('v1')
+    .middleware(middleware.auth())
+}
