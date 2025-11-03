87acb81c7e3fbbeac22dcee34aaa0e9c780c456c
alexandrebrgn <brgn.alexandre@gmail.com>
2025-11-03 12:34:17 +0100

    feat(policy) : exercise policy

--- backend/app/domains/exercise/controllers/exercises_controller.ts
+++ backend/app/domains/exercise/controllers/exercises_controller.ts
@@ -2,12 +2,14 @@ import { inject } from '@adonisjs/core'
 import { HttpContext } from '@adonisjs/core/http'
 import ExerciseService from '#domains/exercise/services/exercise_service'
 import { searchExerciseValidator } from '#domains/exercise/validators/exercise_validator'
+import ExercisePolicy from '#domains/exercise/policies/exercise_policy'
 
 @inject()
 export default class ExercisesController {
   constructor(protected exerciseService: ExerciseService) {}
 
-  async index({ request }: HttpContext) {
+  async index({ request, bouncer }: HttpContext) {
+    await bouncer.with(ExercisePolicy).authorize('browse')
     const payload = await request.validateUsing(searchExerciseValidator)
 
     return this.exerciseService.paginate(payload)
