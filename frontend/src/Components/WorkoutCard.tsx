15e6ef7cb2ae36e5db9e6a0922bce21e2ce41c69
PyGaVS <lylian270104@gmail.com>
2025-11-28 11:49:43 +0100

    update css

--- frontend/src/Components/WorkoutCard.tsx
+++ frontend/src/Components/WorkoutCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function WorkoutCard(props: PropsWithChildren<Props>){
   return (
-    <div className="bg-[var(--surface)] shadow-md rounded-[var(--radius)] p-5 m-1 mb-4 w-xl hover:shadow-xl transition-shadow duration-300 border border-[var(--border)]">
+    <div className="bg-surface shadow-md rounded-[var(--radius)] p-5 m-1 mb-4 w-xl hover:shadow-xl transition-shadow duration-300 border border-[var(--border)]">
       <div className="flex justify-between items-center mb-2">
         <h2 className="text-xl font-bold text-text">
           Workout {props.workout.getDate().toLocaleDateString()}
@@ -16,9 +16,9 @@ export default function WorkoutCard(props: PropsWithChildren<Props>){
     
       <div className="flex flex-col gap-3">
         {props.workout.exerciseBlocs.map((bloc) => (
-          <div key={bloc.id} className="bg-[var(--bg)] p-3 rounded-md border-l-4 border-primary">
+          <div key={bloc.id} className="bg-bg p-3 rounded-md border-l-4 border-primary">
             <h3 className="font-semibold text-secondary">{bloc.title}</h3>
-              <ul className="mt-1 text-[var(--text-muted)] text-sm list-disc list-inside">
+              <ul className="mt-1 text-text-muted text-sm list-disc list-inside">
                 {bloc.sets.map((set) => (
                   <li key={set.id}>
                     <span className="font-medium text-text">{set.exercise.name}</span> {set.reps} reps @ {set.weight} kg
--- frontend/src/views/addWorkout.tsx
+++ frontend/src/views/addWorkout.tsx
@@ -9,12 +9,12 @@ export default function AddWorkout() {
   return (
     <Dialog modal>
       <DialogTrigger asChild>
-        <button className="bg-secondary text-[var(--surface)] inline-block px-2 py-3 rounded-[var(--radius)] my-3 mx-6 border-none
-          shadow-md transition-all duration-300 hover:bg-[var(--accent)] hover:shadow-lg hover:scale-105">
+        <button className="bg-secondary text-surface inline-block px-2 py-3 rounded-radius my-3 mx-6 border-none
+          shadow-md transition-all duration-300 hover:bg-accent hover:shadow-lg hover:scale-105">
           <i className="fa-solid fa-dumbbell fa-xl pr-1" /> Save workout
         </button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-5xl bg-[var(--surface)]">
+      <DialogContent className="sm:max-w-5xl bg-surface">
         azdefergvd
       </DialogContent>
     </Dialog>
